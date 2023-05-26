a5051715a24568f62aff832f9aed27d179047b17
Donald Yu <longsheng.yu@lego.com>
2023-05-26 15:56:37 +0800

    Update Program.cs

--- WebApp1/WebApp2/Program.cs
+++ WebApp1/WebApp2/Program.cs
@@ -17,11 +17,8 @@ namespace WebApp2
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
+            app.UseSwagger();
+            app.UseSwaggerUI();
 
             app.UseAuthorization();
 
