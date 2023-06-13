ba2a10b6ab81c33c427a9874ea4016c43bea758a
Donald Yu <128213483+donald4yls@users.noreply.github.com>
2023-06-13 21:23:01 +0800

    Update Startup.cs

--- OpenIdDemoV1/Startup.cs
+++ OpenIdDemoV1/Startup.cs
@@ -101,7 +101,7 @@ namespace OpenIdDemoV1
             }
             else
             {
-                app.UseStatusCodePagesWithReExecute("~/error");
+                app.UseStatusCodePagesWithReExecute("/home/error");
                 //app.UseExceptionHandler("~/error");
 
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
